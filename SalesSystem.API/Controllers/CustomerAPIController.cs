c518a85283862b46b88f04f554f69545fc29e032
Thangamani Eraniyan <talk2gold@live.com>
2023-11-19 21:05:16 -0800

    Add project files.

---
+++ SalesSystem.API/Controllers/CustomerAPIController.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using SalesSystem.API.Model;
+using System.Data;
+using System.Xml.Linq;
+
+namespace SalesSystem.API.Controllers
+{
+    [Route("api/CustomerAPI")]
+
+    [ApiController]
+	public class CustomerAPIController: ControllerBase
+	{
+
+
+        private IConfiguration _config;
+		private IDbConnection _dbcon;
+		public CustomerAPIController(IConfiguration config)
+        {
+			_config = config;
+			_dbcon = new OracleConnection(_config.GetConnectionString("DefaultConnection"));
+		}
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            
+            string sql = @"Select 
+                              CUSTOMER_ID CustomerId, 
+                              FNAME       FirstName, 
+                              LNAME       LastName, 
+                              PHONENR     PhoneNr, 
+                              CREATED_DT  CreatedDt, 
+                              EMAIL       Email
+                           from Customers order by Customer_id desc";
+            Console.WriteLine(sql);
+            IEnumerable<Customer> returnList = _dbcon.Query<Customer>(sql);
+            return Ok( returnList);
+	}
+	    [HttpGet("custid", Name = "GetCustomerByID")]
+        public ActionResult<Customer> GetCustomerByID(int id)
+        {
+
+            string sql = @"Select 
+                              CUSTOMER_ID CustomerId, 
+                              FNAME       FirstName, 
+                              LNAME       LastName, 
+                              PHONENR     PhoneNr, 
+                              CREATED_DT  CreatedDt, 
+                              EMAIL       Email
+                           from Customers where CUSTOMER_ID = " + id.ToString();
+
+            Customer customer = _dbcon.QuerySingle<Customer>(sql);
+            return Ok(customer);
+        }
+        [HttpPost]
+        public ActionResult AddCustomer( Customer customer)
+        {
+            string sql = @" insert into Customers(FNAME, LNAME, PHONENR,EMAIL) VALUES("
+                + "' " + customer.FirstName 
+				+ "', '" + customer.LastName 
+				+ "', '" + customer.PhoneNr 
+				+ "', '" + customer.Email + "')";
+
+            int recCnt =_dbcon.Execute(sql);
+            if (recCnt == 0)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok();
+            }
+
+        }
+	}
+}
---
+++ SalesSystem.API/Model/Customer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace SalesSystem.API.Model
+{
+	public class Customer
+	{
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string PhoneNr  { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDt { get; set; }
+
+        public Customer()
+        {
+
+            if (FirstName == null)
+            {
+                FirstName = string.Empty;
+            }
+			if (LastName == null)
+			{
+				LastName = string.Empty;
+			}
+			if (PhoneNr == null)
+			{
+				PhoneNr = string.Empty;
+			}
+			if (Email == null)
+			{
+				Email = string.Empty;
+			}
+		}
+	}
+}
